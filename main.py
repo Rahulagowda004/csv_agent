824e51b90831c6290b7c75deee6af7b07768afb6
satvik_gs <satvik.gs@thoughtclan.com>
2025-09-13 13:54:51 +0530

    added starter files

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from csv-agent!")
+from langchain_openai import ChatOpenAI
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from dotenv import load_dotenv
+from langchain.agents.agent_types import AgentType
+load_dotenv()
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
+agent_executor = create_csv_agent(
+    llm,
+    "data.csv",
+    # agent_type="zero-shot-react-description",
+    verbose=True,
+    allow_dangerous_code=True,
+    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True
+)
+
+response = agent_executor.run("Show monthly sales trend for 2023.")
+
+print(response)
 
 
-if __name__ == "__main__":
-    main()
